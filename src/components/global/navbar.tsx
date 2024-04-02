4b31bc316dfa137f6eef7c9549c53d78f65770b2
Abhishek suman <abhisheksumancode@gmail.com>
2024-04-03 01:13:00 +0530

    AutoCommand project set up v0.1

---
+++ src/components/global/navbar.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+type Props = {}
+
+const Navbar = (props: Props) => {
+    return (
+        <header className='fixed right-0 left-0 top-0 py-4 px-4 
+        bg-black/40 backdrop-blur-lg z-[100] flex items-center
+        border-b-[1px] border-neutral-900 justify-between
+        '>
+
+
+            <aside>
+                <p></p>
+                <img src="" alt="" />
+                <p></p>
+            </aside>
+
+
+        </header>
+    )
+}
+
+export default Navbar
